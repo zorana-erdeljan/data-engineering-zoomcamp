205eb753aa30f921ec6ba6aa46fa4178fbf27d7e
zorana-erdeljan <97516486+zorana-erdeljan@users.noreply.github.com>
2026-01-21 20:30:11 +0000

    pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,18 @@
+import sys
+import pandas as pd
+
+
+print ('Hello pipeline')
+print ('Arguments', sys.argv)
+month = int(sys.argv[1])
+print (f'Current Month ={month}')
+
+df = pd.DataFrame({"Day ": [1, 2], "Num of passangers": [3, 4]})
+df['month'] = month
+print(df.head())
+
+df.to_parquet(f"output_{month}.parquet_")
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
+
+
