75d8f3e4d154f6753401d7eb999e435ee7e532d8
zorana-erdeljan <97516486+zorana-erdeljan@users.noreply.github.com>
2026-02-04 06:59:57 +0000

    Docker + postgress

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -98,25 +98,94 @@ parse_dates = [
 
 
 # In[18]:
-def run():
+# def run():
 
-    pg_name= 'root'
-    pg_pass= 'root'
-    pg_host = 'localhost'
-    pg_port= 5432
-    pg_db= 'ny_taxi'
+#     pg_name= 'root'
+#     pg_pass= 'root'
+#     pg_host = 'localhost'
+#     pg_port= 5432
+#     pg_db= 'ny_taxi'
 
-    year = 2021
-    month = 1
+#     year = 2021
+#     month = 1
 
-    targer_table = 'yellow_taxi_data'
+#     targer_table = 'yellow_taxi_data'
 
-    chunck_size = 100000
+#     chunck_size = 100000
 
+#     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
+#     url = f'{prefix}/yellow_tripdata_2021-01.csv.gz'
+#     engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+
+
+#     df_iter = pd.read_csv(
+#         url,
+#         dtype=dtype,
+#         parse_dates=parse_dates,
+#         iterator=True,
+#         chunksize=chunck_size
+#     )
+
+#     first = True
+
+#     for df_chunk in tqdm(df_iter):
+
+#         if first:
+#             # Create table schema (no data)
+#             df_chunk.head(0).to_sql(
+#                 name=targer_table,
+#                 con=engine,
+#                 if_exists="replace"
+#             )
+#             first = False
+#             print("Table created")
+
+#         # Insert chunk
+#         df_chunk.to_sql(
+#             name=targer_table,
+#             con=engine,
+#             if_exists="append"
+#         )
+
+#         print("Inserted:", len(df_chunk))
+
+
+
+# if __name__ == '__main__':
+#     run()
+
+
+# print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
+
+# df.head(0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
+
+# get_ipython().system('uv add tqdm')
+
+# for df_chunk in tqdm(df_iter):
+#     df_chunk.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
+
+
+
+
+
+import click
+
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table):
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
     url = f'{prefix}/yellow_tripdata_2021-01.csv.gz'
-    engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+    
+    conn_str = f"postgresql+psycopg2://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}"
+    engine = create_engine(conn_str)
 
+   # engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+    chunck_size = 100000
 
     df_iter = pd.read_csv(
         url,
@@ -133,7 +202,7 @@ def run():
         if first:
             # Create table schema (no data)
             df_chunk.head(0).to_sql(
-                name=targer_table,
+                name=target_table,
                 con=engine,
                 if_exists="replace"
             )
@@ -142,7 +211,7 @@ def run():
 
         # Insert chunk
         df_chunk.to_sql(
-            name=targer_table,
+            name=target_table,
             con=engine,
             if_exists="append"
         )
@@ -153,15 +222,3 @@ def run():
 
 if __name__ == '__main__':
     run()
-
-
-# print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
-
-# df.head(0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
-
-# get_ipython().system('uv add tqdm')
-
-# for df_chunk in tqdm(df_iter):
-#     df_chunk.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
-
-
